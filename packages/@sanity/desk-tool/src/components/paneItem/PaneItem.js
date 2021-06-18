b5fa5527475aaa62fc97396fed400a22e56842a5
hermanwikner <wiknerherman@hotmail.com>
2021-06-18 11:18:43 +0200

    feat(base): add updated PaneItem component

--- packages/@sanity/desk-tool/src/components/paneItem/PaneItem.js
+++ packages/@sanity/desk-tool/src/components/paneItem/PaneItem.js
@@ -1,6 +1,7 @@
-import PropTypes from 'prop-types'
 import React, {useCallback, useContext} from 'react'
-import {Card} from '@sanity/ui'
+import PropTypes from 'prop-types'
+import {Card, Text, Box} from '@sanity/ui'
+import {ChevronRightIcon} from '@sanity/icons'
 import schema from 'part:@sanity/base/schema'
 import {SanityDefaultPreview} from 'part:@sanity/base/preview'
 import folderIcon from 'part:@sanity/base/folder-icon'
@@ -8,60 +9,13 @@ import fileIcon from 'part:@sanity/base/file-icon'
 import DocumentPaneItemPreview from '../../components/DocumentPaneItemPreview'
 import getIconWithFallback from '../../utils/getIconWithFallback'
 import MissingSchemaType from '../../components/MissingSchemaType'
-// import PaneItemWrapper from './PaneItemWrapper'
 import {PaneRouterContext} from '../../contexts/PaneRouterContext'
-
-export default function PaneItem(props) {
-  const {id, isSelected, schemaType, layout, icon, value} = props
-  // const useGrid = layout === 'card' || layout === 'media'
-  const {ChildLink} = useContext(PaneRouterContext)
-  const hasSchemaType = schemaType && schemaType.name && schema.get(schemaType.name)
-
-  let preview
-  if (value && value._id) {
-    preview = hasSchemaType ? (
-      <DocumentPaneItemPreview
-        icon={getIconWithFallback(icon, schemaType, fileIcon)}
-        layout={layout}
-        schemaType={schemaType}
-        value={value}
-      />
-    ) : (
-      <MissingSchemaType value={value} />
-    )
-  } else {
-    preview = (
-      <SanityDefaultPreview
-        icon={getIconWithFallback(icon, schemaType, folderIcon)}
-        layout={layout}
-        value={value}
-      />
-    )
-  }
-
-  const LinkComponent = useCallback(
-    (linkProps) => {
-      return <ChildLink {...linkProps} childId={id} />
-    },
-    [ChildLink, id]
-  )
-
-  // Todo: figure out what to do with useGrid and layout
-  return (
-    // <PaneItemWrapper id={id} isSelected={isSelected} layout={layout} useGrid={useGrid}>
-    //   {preview}
-    // </PaneItemWrapper>
-
-    <Card padding={2} data-as="a" radius={3} selected={isSelected} as={LinkComponent}>
-      {preview}
-    </Card>
-  )
-}
 
 PaneItem.propTypes = {
   id: PropTypes.string.isRequired,
   layout: PropTypes.string,
   isSelected: PropTypes.bool,
+  isActive: PropTypes.bool,
   icon: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
   value: PropTypes.shape({
     _id: PropTypes.string,
@@ -81,5 +35,72 @@ PaneItem.defaultProps = {
   icon: undefined,
   value: null,
   isSelected: false,
+  isActive: false,
   schemaType: null,
+}
+
+export default function PaneItem(props) {
+  const {id, isSelected, schemaType, layout, icon, value, isActive} = props
+  const {ChildLink} = useContext(PaneRouterContext)
+  const hasSchemaType = schemaType && schemaType.name && schema.get(schemaType.name)
+
+  let preview
+  if (value && value._id) {
+    preview = hasSchemaType ? (
+      <DocumentPaneItemPreview
+        icon={getIconWithFallback(icon, schemaType, fileIcon)}
+        layout={layout}
+        schemaType={schemaType}
+        value={value}
+      />
+    ) : (
+      <MissingSchemaType value={value} />
+    )
+  } else {
+    preview = (
+      <SanityDefaultPreview
+        status={
+          <Box padding={1}>
+            <Text style={{color: 'inherit', opacity: 0.75}} size={2}>
+              <ChevronRightIcon />
+            </Text>
+          </Box>
+        }
+        icon={getIconWithFallback(icon, schemaType, folderIcon)}
+        layout={layout}
+        value={value}
+      />
+    )
+  }
+
+  const LinkComponent = useCallback(
+    (linkProps) => {
+      return <ChildLink {...linkProps} childId={id} />
+    },
+    [ChildLink, id]
+  )
+
+  if (isSelected && isActive) {
+    return (
+      <div aria-selected>
+        <Card padding={2} data-as="a" radius={2}>
+          {preview}
+        </Card>
+      </div>
+    )
+  }
+
+  if (isSelected) {
+    return (
+      <Card padding={2} data-as="a" radius={2} selected>
+        {preview}
+      </Card>
+    )
+  }
+
+  return (
+    <Card padding={2} data-as="a" radius={2} as={LinkComponent}>
+      {preview}
+    </Card>
+  )
 }
